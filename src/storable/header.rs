0d851d0f5e66c7c377dac127d18b8c2777123813
Deeksha Chitale <deeksha.chitale@gmail.com>
2026-07-14 20:46:35 +0530

    Version byte

--- src/storable/header.rs
+++ src/storable/header.rs
@@ -1,4 +1,40 @@
+#[derive(Debug,PartialEq)]
 pub struct VersionByte {
     pub major: u8,
     pub network_order: bool,
+}
+
+pub fn parse_version_byte(b: u8) -> VersionByte {
+    VersionByte { 
+        major: b >> 1, 
+        network_order: (b & 0x01) != 0 
+    }
+}
+
+#[cfg(test)] // the following module is compiled only when running tests.
+mod tests {
+    use super::*; // imports everything from the parent module.
+    
+    #[test] // first test
+    fn freeze_sets_native_order(){
+        assert_eq!(
+            parse_version_byte(0x04),
+            VersionByte{
+                major: 2,
+                network_order: false
+            }
+        );
+    }
+    
+    #[test] // second test
+    fn nfreeze_sets_network_order(){
+        assert_eq!(
+            parse_version_byte(0x05),
+            VersionByte{
+                major: 2,
+                network_order: true
+            }
+        );
+    }
+
 }
